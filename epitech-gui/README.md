b7adee1e6402bbb9c5e3133b34edd9454e172700
Samuel Berthe <dev@samuel-berthe.fr>
2019-01-10 15:35:17 +0100

    epitech-gui: adding web client (novnc)

--- epitech-gui/README.md
+++ epitech-gui/README.md
@@ -18,13 +18,21 @@ Install a VNC client (such as [VNC Viewer](https://www.realvnc.com/en/connect/do
 $ cd student-project/
 $ fedora-gui
 Starting VNC Server
-OK
+VNC Server started
 
-@@@@@@@@@@@@@@@@@@@@@@@
+Starting VNC web client on port 5980
+VNC web client started on port 5980
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
  VNC PASSWORD: epitech
-@@@@@@@@@@@@@@@@@@@@@@@
+ Web client: http://localhost:5980
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 [root@246805b5e9dc app]# java -cp target/currency-converter-1.0.0.jar Main
 ```
 
 From your VNC client, connect to localhost:5900 with "epitech" password.
+
+## Web client
+
+Just open [http://localhost:5980](http://localhost:5980) ;)
--- epitech-gui/start_vnc.sh
+++ epitech-gui/start_vnc.sh
@@ -4,19 +4,28 @@ set -e
 #set -x
 
 export DISPLAY=:0
+
 export PASS=${PASS:-epitech}
+export PORT=${PORT:-5980}
 
 echo "Starting VNC Server"
 
 printf "${PASS}\n${PASS}\n\n" | vncpasswd > /dev/null 2>&1
 vncserver ${DISPLAY} -geometry 1920x1080 > /dev/null 2>&1
 
-echo "OK"
+echo "VNC Server started"
+echo
+echo "Starting VNC web client on port ${PORT}"
+
+/usr/src/app/noVNC/utils/launch.sh --listen ${PORT} > /dev/null 2>&1 &
+
+echo "VNC web client started on port ${PORT}"
 
 echo
-echo "@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " VNC PASSWORD: ${PASS}"
-echo "@@@@@@@@@@@@@@@@@@@@@@@"
+echo " Web client: http://localhost:5980"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo
 
 exec $@
